2a01273a98cf609e0f4c3298f2e726425f261f32
syedfaheem27 <syedfaheem2719@gmail.com>
2024-03-26 17:07:55 +0530

    update classname for country card

--- XCountrySearch/src/components/CountryCard.jsx
+++ XCountrySearch/src/components/CountryCard.jsx
@@ -1,6 +1,6 @@
 const Card = ({ flag, name }) => {
     return (
-        <div className="card">
+        <div className="countryCard">
             <img src={flag} alt={`Flag of ${name}`} />
             <h4>{name}</h4>
         </div>
